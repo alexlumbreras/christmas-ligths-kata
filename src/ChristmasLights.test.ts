89ccddced12f79717679128a4f2361707f6511a1
Endika Orube <endika.orube@biko2.com>
2024-12-11 15:44:01 +0100

    Init

---
+++ src/ChristmasLights.test.ts
@@ -0,0 +1,9 @@
+import { ChristmasLights } from "./ChristmasLights";
+
+describe("ChristmasLights", () => {
+  it("should ", () => {
+    const christmasLights = new ChristmasLights();
+
+    expect(christmasLights).toBeFalsy();
+  });
+});
