8ecc0394f4d86055c013f18736b77f27dae095b7
Alejandro Lumbreras López <alexlumbreras@MacBook-Pro-de-Alejandro.local>
2024-12-15 17:45:13 +0100

    refactor: implement getLightsOn with board initialization

--- src/ChristmasLightsBoard.ts
+++ src/ChristmasLightsBoard.ts
@@ -1,5 +1,16 @@
 export class ChristmasLightsBoard {
+  static readonly COLUMNS = 100;
+  public christmasLightsBoard: number[];
+
+  constructor() {
+    this.christmasLightsBoard = new Array(ChristmasLightsBoard.COLUMNS).fill(0);
+  }
+
   public getLightsOn(): number {
-    return 0;
+    const christmasLights = this.christmasLightsBoard.filter(
+      (column) => column === 1
+    ).length;
+
+    return christmasLights;
   }
 }
